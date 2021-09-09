70c63ffe90a7b01b071731507728d4f7676e3134
江夏哲 <784985696@qq.com>
2021-09-09 15:57:23 +0800

    邮件发送test

--- test/test.py
+++ test/test.py
@@ -1,15 +1,34 @@
 from basiclib.common_util import get_base_disk_dir
 from basiclib.mail_util import MailServer
+import smtplib
+from email.mime.text import MIMEText
 
 
 def test_mail_send():
     # todo 此处可写代码验证是否邮件能否发送成功，非必须
-    test_mail_addr = "276095361@qq.com"
-    mail_content = str.format('your verify code is %s', "123456")
-    mail_server = MailServer()
-    mail_server.send_verify_mail(mail_content, test_mail_addr)
+    # 发送邮件相关参数
+    smtpserver = 'smtp.163.com'  # 发件服务器
+    port = 25  # 端口
+    sender = '17328618471@163.com'  # 发件人邮箱
+    psw = 'EKYKJABHUUBFDJTN'  # 发件人密码
+    receiver = "784985696@qq.com"  # 接收人
+
+    #编写邮件主题和正文，正文用的html格式
+    subject = '邮件主题'
+    body = '<p>这是个发送邮件的正文</p>'  # 定义邮件正文为html
+    msg = MIMEText(body, 'html', 'utf-8')
+    msg['from'] = sender
+    msg['to'] = receiver
+    msg['subject'] = subject
+
+    # 发送邮件
+    smtp = smtplib.SMTP()
+    smtp.connect(smtpserver)  # 链接服务器
+    smtp.login(sender, psw)  # 登录
+    smtp.sendmail(sender, receiver, msg.as_string())  # 发送
+    smtp.quit()  # 关闭
 
 
 if __name__ == '__main__':
-  #  str_dir=get_base_disk_dir()
+    #  str_dir=get_base_disk_dir()
     test_mail_send()
